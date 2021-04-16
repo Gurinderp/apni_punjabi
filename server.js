4f5de8c6c1b340acfb46772bd1b856bd691980c1
rednirug <sgurinderpreet@gmail.com>
2021-04-16 11:52:47 -0700

    added db and port connections

--- server.js
+++ server.js
@@ -6,5 +6,18 @@ dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const env = process.env.DB_URI;
+
+mongoose
+	.connect(env, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+	})
+	.then((result) => console.log("Connected to DB!"))
+	.catch((err) => console.log(err));
+
+app.listen(PORT, () => {
+	console.log(`app listening on port: ${PORT}`);
+});
